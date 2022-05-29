84ac60ed3c98e0565a588ac4eebaf7be08b0baba
snailcreature <samuel.drage@myport.ac.uk>
2022-05-29 15:01:33 +0100

    Added basic assertion

--- demo/index.js
+++ demo/index.js
@@ -3,10 +3,14 @@
  * @param {String} name - Name of the person to greet
  * @returns {String}
  * 
- * @assert This is a test
+ * @assert {Assertion} - John=>Hello, John!
  */
 function greet(name) {
   return `Hello, ${name}!`
 }
 
 console.log(greet("John"));
+
+module.exports = {
+  greet,
+}
--- plugin/assertivedocs.js
+++ plugin/assertivedocs.js
@@ -1,6 +1,30 @@
+function Assertion(func, args, expected) {
+  this.func = func ? func : () => {};
+  this.args = args;
+  this.expected = expected;
+
+  this.assert = function() {
+    try {
+      const outcome = this.func(...this.args);
+      return outcome === this.expected;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
 function assertOnTagged(doclet, tag) {
-  console.log(tag);
   tag.value.type ? console.log(tag.value.type) : console.log("No type");
+
+  //let funcName = eval(doclet.meta.code.name);
+  const file = require(`${doclet.meta.path}/${doclet.meta.filename}`);
+
+  const parts = tag.value.description.split('=>');
+  const args = parts[0].split(',');
+  const expected = parts[1];
+  const test = new Assertion(file[doclet.meta.code.name], args, expected);
+
+  console.log(test.assert());
 }
 
 exports.defineTags = function(dictionary) {
@@ -8,9 +32,7 @@ exports.defineTags = function(dictionary) {
   dictionary.defineTag('assert', {
     mustHaveValue: true,
     canHaveType: true,
+    canHaveName: true,
     onTagged: assertOnTagged,
   });
-}
-
-exports.handlers = {
 }
